42e20c1cfa16661eff8488e451b84a267d4ede27
KevinG <kevg5534@gmail.com>
2019-04-11 13:59:23 -0400

    added documentation

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from  bs4 import BeautifulSoup
 import html.parser
 import requests
 import re
+#for url validation
 regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
@@ -10,6 +11,7 @@ regex = re.compile(
         r'(?::\d+)?' # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
+#master list of urls
 linklist = dict()
 
 def soupify (page):
@@ -22,22 +24,25 @@ def soupify (page):
             links.append(a['href'])
     return links
 
-def pagegetter (url): 
+def pagegetter (url):
+    """takes in url returns webpage""" 
     response= requests.get(url)
     print(response)
     return response.text
 
 def linkmanager (arr):
+    """takes in an array of links and adds to master link list"""
     for link in arr:
         if linklist.get(link)==None:
             linklist[link]={'visited':False}
     
-
+#gets espn.com
 current_page=pagegetter("http://espn.com")
-
+#gets all validated links from espn.com
 returned_links = soupify(current_page)
+#adds links to master list
 linkmanager (returned_links)
-
+#visits each link from main page
 for link in (returned_links):
     page = pagegetter (link)
     returned_links =soupify(page)
